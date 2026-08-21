a8be5121dfc5183ce2ce1f1bd00df27abdd6f682
ramadoka <homu@ramadoka.com>
2026-08-21 19:31:34 +0700

    chore: replace test against full image to partial image

--- src/pkg/exif/comfyui.rs
+++ src/pkg/exif/comfyui.rs
@@ -126,7 +126,7 @@ mod tests {
 
     #[test]
     fn can_extract_exif() -> std::io::Result<()> {
-        let data = std::fs::read("./samples/inputs/images/saber.comfy.png")?;
+        let data = std::fs::read("./samples/inputs/mocks/saber.comfy.png")?;
         let exif = Exif::<ComfyUI>::new(data);
         let text = exif.text();
         print!("raw: {}\n", text);
@@ -149,7 +149,7 @@ mod tests {
 
     #[test]
     fn invalid_doesnot_panic() -> std::io::Result<()> {
-        let data = std::fs::read("./samples/inputs/images/elysia.a1111.png")?;
+        let data = std::fs::read("./samples/inputs/mocks/elysia.a1111.png")?;
         let exif = Exif::<ComfyUI>::new(data);
         let text = exif.text();
         assert_eq!(text, "");
